12622e25c56a851985d7f50569048e536acd8a9c
JohnQ1981 <ikram1981@gmail.com>
2023-01-11 21:35:48 -0600

    added logos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import "./App.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+//console.log(AlexaImage,CortanaImage, SiriImage);
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <div>Personal Digital Assistants</div>
       <hr />
       <hr />
       <div>
         {" "}
-        <ProfileCard title="Alexa" handle="@alexa99" platform ="Amazon" color ="blue" />
+        <ProfileCard title="Alexa" handle="@alexa99" platform ="Amazon" color ="blue"image={AlexaImage} />
       </div>
       <hr />
       <div>
         {" "}
-        <ProfileCard title="Cortana" handle="@cortana32" platform ="Windows" color ="green" />
+        <ProfileCard title="Cortana" handle="@cortana32" platform ="Windows" color ="green" image={CortanaImage} />
       </div>
       <hr />
       <div>
         {" "}
-        <ProfileCard title="Siri" handle="@siri01" platform ="Apple" color ="red" />
+        <ProfileCard title="Siri" handle="@siri01" platform ="Apple" color ="red" image={SiriImage} />
       </div>
     </div>
   );
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,9 @@
-function ProfileCard ({title, handle, platform, color}){
+function ProfileCard ({title, handle, platform, color , image}){
     const style ={color:color};
     //const {title, handle, platform} = props;
     return (
-        <div> 
+        <div className ="App"> 
+            <img src={image} alt="Logo images"/>
             <div><h1 style ={style}>Title is {title}.</h1></div>
             <div>Handle is {handle}.</div>
             <div>Platform it uses is {platform}.</div>
