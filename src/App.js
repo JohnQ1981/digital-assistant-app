fe1ce63228b682863a95558fbdde2e4527aa358c
JohnQ1981 <ikram1981@gmail.com>
2023-01-13 21:04:29 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import "./App.css";
 import ProfileCard from "./ProfileCard";
-import AlexaImage from './images/alexa.png';
-import CortanaImage from './images/cortana.png';
-import SiriImage from './images/siri.png';
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+import "bulma/css/bulma.css";
 
 //console.log(AlexaImage,CortanaImage, SiriImage);
 
 function App() {
   return (
-    <div className ="App">
-      <div>Personal Digital Assistants</div>
-      <hr />
-      <hr />
-      <div>
-        {" "}
-        <ProfileCard title="Alexa" handle="@alexa99" platform ="Amazon" color ="blue"image={AlexaImage} />
-      </div>
-      <hr />
-      <div>
-        {" "}
-        <ProfileCard title="Cortana" handle="@cortana32" platform ="Windows" color ="green" image={CortanaImage} />
-      </div>
-      <hr />
-      <div>
-        {" "}
-        <ProfileCard title="Siri" handle="@siri01" platform ="Apple" color ="red" image={SiriImage} />
+    <div>
+      <section className="hero is-primary">
+        <div className="her-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      
+     
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                platform="Amazon"
+                color="blue"
+                image={AlexaImage}
+              />
+            </div>
+
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                platform="Windows"
+                color="green"
+                image={CortanaImage}
+              />
+            </div>
+
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                platform="Apple"
+                color="red"
+                image={SiriImage}
+              />
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,14 +1,23 @@
-function ProfileCard ({title, handle, platform, color , image}){
-    const style ={color:color};
-    //const {title, handle, platform} = props;
-    return (
-        <div> 
-            <img src={image} alt="Logo images"/>
-            <div><h1 style ={style}>Title is {title}.</h1></div>
-            <div>Handle is {handle}.</div>
-            <div>Platform it uses is {platform}.</div>
-
+function ProfileCard({ title, handle, platform, color, image }) {
+  const style = { color: color };
+  //const {title, handle, platform} = props;
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="Logo images" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+        <p  className="title is-4">
+          <h1 style={style}>Title is {title}.</h1>
+        </p>
+        <p className="subtitle is-6">Handle is {handle}.</p>
+        <div className="content"><p className="subtitle is-8">Platform it uses is {platform}.</p></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 export default ProfileCard;
